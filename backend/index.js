d103af2e09475288435d3bc26e306bc5e8381354
unknown <rohit.valsetwar@rbhuin.onmicrosoft.com>
2025-08-14 12:52:38 +0530

    node.js bassic application setup and learnings

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const { createServer } = require('node:http');
 
-const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Test Route
-app.get("/", (req, res) => {
-  res.send("Backend chal raha hai 🔥");
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
-// Port
-const PORT = process.env.PORT || 5000;
-
-// Start Server
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
